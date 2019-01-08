7ff9080a81088bbdb44878a46d1e78ab31c16801
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-01-08 12:58:24 +0530

    Author             : Prateek File Modified      :DbScript.sql Description        : Changed database name from nskfdc to scgj_pmts

--- Database/DBScript/DbScript.sql
+++ Database/DBScript/DbScript.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS scgj_pmts;
 GRANT ALL PRIVILEGES ON *.* TO 'pmts_db_user'@'localhost' IDENTIFIED BY 'pMtS1234$$' with GRANT option;
 GRANT SELECT  ON `scgj_pmts`.* TO 'pmts_user'@'localhost' IDENTIFIED BY 'myteam';
-GRANT SELECT,UPDATE,INSERT,DELETE  ON `nskfdc`.* TO 'pmts_app_user'@'localhost' IDENTIFIED BY 'pMtS@123$$#';
+GRANT SELECT,UPDATE,INSERT,DELETE  ON `scgj_pmts`.* TO 'pmts_app_user'@'localhost' IDENTIFIED BY 'pMtS@123$$#';
 
