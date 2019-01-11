4093b752ad0537843b0f85c4655fc93a4cfba7c7
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-01-11 16:47:56 +0530

    Author           : Prateek Kapoor File Modified    : DataEntryOperatorSearchApplicationDao.java Description      : This file contains methods to search the application id using 3 different search parameters -
    1. Application Id
    2. Department Names
    3. Date Range

---
+++ postManagementTrackingSystem/postManagementTrackingSystem-common/src/main/java/com/postManagementTrackingSystem/scgj/dao/DataEntryOperatorSearchApplicationDao.java
@@ -0,0 +1,135 @@
+package com.postManagementTrackingSystem.scgj.dao;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.postManagementTrackingSystem.scgj.common.AbstractTransactionalDao;
+import com.postManagementTrackingSystem.scgj.config.DataEntryOperatorSearchApplicationConfig;
+import com.postManagementTrackingSystem.scgj.dto.DataEntryOperatorApplicationSearchResultsDto;
+
+@Repository
+public class DataEntryOperatorSearchApplicationDao extends AbstractTransactionalDao{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(DataEntryOperatorSearchApplicationDao.class);
+	
+	@Autowired
+	private DataEntryOperatorSearchApplicationConfig dataEntryOperatorSearchApplicationConfig;
+	
+	private static ApplicationDetailsRowMapper ROW_MAPPER = new ApplicationDetailsRowMapper();
+	
+	public Collection<DataEntryOperatorApplicationSearchResultsDto> getApplicationByApplicationId(String applicationId)
+	{
+		LOGGER.debug("Request received from DataEntryOperatorSearchApplicationService class to search application details using application id: "+applicationId);
+		LOGGER.debug("Creating hashmap of objects");
+		Map<String,Object>applicationIdParam = new HashMap<>();
+		LOGGER.debug("Hashmap of object created successfully");
+		LOGGER.debug("Inserting application id: "+applicationId);
+		applicationIdParam.put("applicationId", applicationId);
+		LOGGER.debug("Parameter inserted into the hashmap");
+		
+		try
+		{
+			LOGGER.debug("In try block of getApplicationByApplicationId method");
+			LOGGER.debug("Executing query to get the details of the application with application-id : "+applicationId);
+			return getJdbcTemplate().query(dataEntryOperatorSearchApplicationConfig.getSearchApplicationByApplicationId(),applicationIdParam, ROW_MAPPER);
+		}
+		catch(Exception e)
+		{
+			LOGGER.error("An exception occured while fetching the record of application using application Id: "+e);
+			LOGGER.debug("Returning NULL");
+			return null;
+		}
+		
+	}
+	
+	
+	
+	public Collection<DataEntryOperatorApplicationSearchResultsDto> getApplicationByDepartment(String department)
+	{
+		LOGGER.debug("Request received from service to get application details by department name: "+department);
+		LOGGER.debug("Creating Hashmap of objects");
+		Map<String,Object>params = new HashMap<>();
+		LOGGER.debug("Hashmap created successfully");
+		LOGGER.debug("Putting values into the hashmap");
+		params.put("department", department);
+		LOGGER.debug("Values inserted successfully");
+		try {
+			
+			LOGGER.debug("In try block of getApplicationByDepartment method to get application details using department name: "+department);
+			LOGGER.debug("Executing query to get the record of application on the basis of department");
+			return getJdbcTemplate().query(dataEntryOperatorSearchApplicationConfig.getSearchApplicationByDepartmentName(), params, ROW_MAPPER);
+			
+		} catch (Exception e) {
+			LOGGER.error("An exception occured while fetching record of the application using department name: "+department);
+			LOGGER.error("The exception is: "+e);
+			LOGGER.error("Returning NULL");
+			return null;
+		}
+	}
+	
+	
+	public Collection<DataEntryOperatorApplicationSearchResultsDto> getApplicationByDateRange(String startingDate, String endingDate)
+	{
+		LOGGER.debug("Request received in getApplicationByDateRange to get the application received between: "+startingDate+" to: "+endingDate);
+		LOGGER.debug("Creating hashmap of dates");
+		Map<String,Object>param = new HashMap<String,Object>();
+		LOGGER.debug("Hashmap created successfully");
+		LOGGER.debug("Inserting values into hashmap");
+		param.put("startingDate", startingDate);
+		param.put("endingDate", endingDate);
+		LOGGER.debug("Parameters successfully inserted into hashmap");
+		try {
+			LOGGER.debug("In try block to get application details using date");
+			LOGGER.debug("Executing query to get application details using date");
+			return getJdbcTemplate().query(dataEntryOperatorSearchApplicationConfig.getSearchApplicationByDateRange(), param, ROW_MAPPER);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			LOGGER.error("An exception occured while fetching application details using date "+e);
+			LOGGER.error("Returning NULL");
+			return null;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static class ApplicationDetailsRowMapper implements RowMapper<DataEntryOperatorApplicationSearchResultsDto>
+	{
+		@Override
+		public DataEntryOperatorApplicationSearchResultsDto mapRow(ResultSet rs, int rowNum) throws SQLException
+		{
+			String applicationId = rs.getString("applicationId");
+			String senderName = rs.getString("senderName");
+			Date dateReceived = rs.getDate("dateReceived");
+			String subject = rs.getString("subject");
+			String priority = rs.getString("priority");
+			String name = rs.getString("name");//name of scgj poc
+			String department = rs.getString("department");
+			String documentPath = rs.getString("documentPath");
+			
+			return new DataEntryOperatorApplicationSearchResultsDto(applicationId,senderName,dateReceived,subject,priority,name,department,documentPath);
+			 
+			 
+			
+			
+		}
+	}
+
+	
+}
