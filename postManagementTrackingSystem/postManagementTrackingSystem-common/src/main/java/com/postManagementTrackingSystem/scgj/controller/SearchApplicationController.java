b6e5b2e5851b0c02161a2e5e97dcb3a8346e447c
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-01-11 19:22:25 +0530

    Author          : Prateek Kapoor File Deleted    : SearchApplicationController.java Description     : Unused controller class - Redundant File

--- postManagementTrackingSystem/postManagementTrackingSystem-common/src/main/java/com/postManagementTrackingSystem/scgj/controller/SearchApplicationController.java
+++
@@ -1,8 +0,0 @@
-package com.postManagementTrackingSystem.scgj.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SearchApplicationController {
-
-}
