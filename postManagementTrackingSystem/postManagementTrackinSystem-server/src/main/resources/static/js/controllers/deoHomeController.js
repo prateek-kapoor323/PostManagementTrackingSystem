98de4d4b0714cf37287e67dc22c6b19a569de16d
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-01-21 23:52:59 +0530

    Author    : Prateek Description : FE DEO - HOme and dynamic binding of DH Name

--- postManagementTrackingSystem/postManagementTrackinSystem-server/src/main/resources/static/js/controllers/deoHomeController.js
+++ postManagementTrackingSystem/postManagementTrackinSystem-server/src/main/resources/static/js/controllers/deoHomeController.js
@@ -2,4 +2,16 @@ var deoHome= angular.module("app");
 
 deoHome.controller("deoHomeController",function($scope, $http){
 	
+	
+	
+	var url = '/getDepartmentHeadsName';
+    $scope.departmentHeadsName = [];
+    $http.get(url)
+	    .then(function(response) {
+	    	console.log(response.data);
+	        let dt = response.data;
+	        for (i in dt) {
+	            $scope.departmentHeadsName.push(response.data[i].departmentHeadName);
+	        }
+	    });
 });
