dcd37f56b5e49919a1eca326085d226b6396e407
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-01-25 09:46:59 +0530

    Author           : Prateek Kapoor File Modified    : insertUsers.sql Description      : Added SQL to insert department heads

--- DBScript/insertUsers.sql
+++ DBScript/insertUsers.sql
@@ -1,2 +1,2 @@
 use scgj_pmts;
-INSERT INTO `scgj_pmts`.`users` (`name`, `email`, `password`, `department`, `role_type`) VALUES ('Manohar Singh', 'manohar@sscgj.in', 'manohar@123', 'Data Entry Operator', 'DEO');
+INSERT INTO `users` VALUES (1,'Arpit Sharma','arpit@sscgj.in','arpit123','Assessment and Assurance','DH'),(2,'Tanmay','tanmay@gmail.com','tanmay123','Standard and Research','DH'),(3,'Manohar','manohar@sscgj.in','manohar','Data Entry Operator','DEO');
