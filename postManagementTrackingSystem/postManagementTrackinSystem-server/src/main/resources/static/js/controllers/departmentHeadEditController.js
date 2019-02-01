53a86156cb0beb4e345c20f9368defd233adc1a6
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-02-01 17:25:58 +0530

    Author          : Prateek Kapoor Files Added     : Functionalities for edit post for department head Description     : Functionalities for Department head edit functionality

--- postManagementTrackingSystem/postManagementTrackinSystem-server/src/main/resources/static/js/controllers/departmentHeadEditController.js
+++ postManagementTrackingSystem/postManagementTrackinSystem-server/src/main/resources/static/js/controllers/departmentHeadEditController.js
@@ -1,5 +1,447 @@
 var departmentHeadEdit = angular.module("app");
 
-departmentHeadEdit.controller("departmentHeadEditController", function ($scope, $http, registerPostService, $timeout) {
+departmentHeadEdit.controller("departmentHeadEditController", function ($scope, $http, $timeout) 
+{
+	
+	
+	
+	var getDepartmentEmployeesName = "/getNameOfDepartmentEmployees";
+	$scope.departmenEmployeesName = [];
 
+	$http.get(getDepartmentEmployeesName)
+	.then(function (response)
+			{
+		
+		let dt = response.data;
+		for (i in dt) {
+			$scope.departmenEmployeesName.push(response.data[i].departmentEmployeeName);
+			
+		}
+	});
+	
+
+	$scope.searchParams={};
+	$scope.searchEditOwner = function()
+	{
+		var url = "/showEditApplicationDetailsDH?applicationId="+$scope.searchParams.applicationId;
+		
+			$http.get(url)
+			.then(function (response) {
+				$scope.searchParams.applicationId = "";
+				
+				if (response.data == null || response.data == "")
+				{
+					$scope.searchErrorMessage = "No Records found";
+					$scope.editOwnerSearchResult.data = [];
+				} 
+				else
+				{
+					$scope.searchErrorMessage = "";
+					$scope.editOwnerSearchResult.data = response.data;
+				}
+	
+			});
+	
+			$timeout(function () {
+				$scope.searchErrorMessage = "";
+			}, 4000);
+		}
+	
+	
+	$scope.editOwnerSearchResult = {
+
+			enableGridMenus: false,
+			enableSorting: false,
+			enableFiltering: false,
+			enableCellEdit: false,
+			enableColumnMenus: false,
+			enableHorizontalScrollbar:1,
+			enableVerticalScrollbar: 1,
+			paginationPageSizes: [5, 10, 20, 30],
+			paginationPageSize: 10,
+			useExternalPagination: true,
+
+			columnDefs: [
+				{
+					name: 'applicationId',
+					displayName: 'Post Id',
+					width:'15%'
+				},
+				{
+					name: 'senderName',
+					displayName: "Sender",
+					width: '15%'
+				},
+				{
+					name: 'dateReceived',
+					displayName: 'Date Received',
+					width: '10%'
+				},
+				{
+					name: 'subject',
+					displayName: 'Subject',
+					width: '15%'
+				},
+				{
+					name: 'priority',
+					displayName: 'Priority',
+					width: '10%'
+						
+				},
+				{
+					name: 'ownerName',
+					displayName: 'Owner',
+					width: '10%'
+				},
+				{
+					name: 'updatedOwner',
+					displayName: 'Update Owner',
+					cellTemplate: 'partials/departmentEmployeesDropDown.html',
+					width: '15%'
+				},
+				{
+					name:'assign',
+					displayName:'Assign',
+					width:'10%',
+					cellTemplate: '<button type="button" class="btn btn-success assignOwnerButton" ng-click="grid.appScope.assignOwner(row)">Assign</button>'
+				},
+				{
+					name: 'dateAssigned',
+					displayName: 'Date Assigned',
+					width: '15%'
+				},
+				{
+					name: 'status',
+					displayName: 'Status',
+					width: '15%'
+				},
+				{
+					name: 'eta',
+					displayName: 'ETA',
+					width: '15%'
+				},
+				{
+					name: 'documentType',
+					displayName: 'Document Type',
+					width: '15%'
+				},
+				{
+					name: 'documentRemarks',
+					displayName: 'Document Remarks',
+					width: '15%'
+				},
+				{
+					name: 'documentPath',
+					displayName: 'Document',
+					width: '10%',
+					cellTemplate: '<img src="images/pdf.png"  ng-click="grid.appScope.downloadPdf(row.entity.documentPath,row.entity.applicationId)" class="pdfIcon" alt="PDF Icon"  class="pointer">'
+				}
+			]
+
+		};
+
+	
+	
+	
+	/** This method is invoked when the application in ON HOLD State is updated to some other state**/
+	$scope.assignOwner=function(row)
+	{
+		
+		if(row.entity.updatedOwner===undefined||row.entity.updatedOwner===null)
+		{
+			$scope.editOwnerSuccessMessage="";
+			$scope.editOwnerErrorMessage="Please select the owner to be assigned";
+			
+		}
+		else
+			{
+		
+			var fd = new FormData();
+			fd.append("applicationId",row.entity.applicationId);
+			fd.append("senderName",row.entity.senderName);
+			fd.append("subject",row.entity.subject);
+			fd.append("priority",row.entity.priority);
+			fd.append("updatedOwner",row.entity.updatedOwner);
+			fd.append("eta",row.entity.eta);
+			fd.append("documentRemarks",row.entity.documentRemarks);
+			fd.append("documentPath",row.entity.documentPath);
+			fd.append("updatedStatus",row.entity.status);
+			fd.append("documentType",row.entity.documentType);
+			
+			var url = "/editApplicationOwnerDH";
+			
+			$http({
+				method: 'POST',
+				url: url,
+				data: fd,
+				headers: {
+					'Content-Type': undefined
+				},
+				transformRequest: angular.identity,
+				transformResponse: [function (data) {
+					thisIsResponse = data;
+					return data;
+
+				}]
+			}).then(function (response)
+						{
+			
+						if(response.data>0)
+							{
+								$scope.editOwnerErrorMessage="";
+								$scope.editOwnerSuccessMessage="Application owner successfully updated";
+								$scope.editOwnerSearchResult.data=[];
+							}
+						else
+							{
+								$scope.editOwnerSuccessMessage ="";
+								$scope.editOwnerErrorMessage = "Application Owner could not be updated";
+
+							}
+				});
+			 
+				
+			
+			}
+		$timeout(function () {
+			$scope.editOwnerSuccessMessage = "";
+			$scope.editOwnerErrorMessage = "";
+		}, 4000);
+		
+		
+	}
+	
+	
+	
+	/** This method takes the document path and application id to download the path by sending request to backend **/
+	$scope.downloadPdf = function (documentPath, applicationId) {
+		var downloadFileUrl = '/downloadFile?filePath=' + documentPath;
+		$http.get(downloadFileUrl, {
+				responseType: 'arraybuffer'
+			})
+			.then(function (response) {
+				if (response.data.byteLength != 0) {
+					var pdfFile = new Blob([response.data], {
+						type: 'application/pdf'
+					})
+					var downloadURL = URL.createObjectURL(pdfFile);
+					var link = document.createElement('a');
+					link.href = downloadURL;
+					link.download = applicationId;
+					document.body.appendChild(link);
+					link.click();
+					
+				} else {
+					$scope.downloadError = "File generated is not a valid PDF file";
+				}
+
+			}, function (error) {
+				$scope.downloadError="";
+				$scope.downloadError = "An error occured while downloading file.";
+
+			});
+		$timeout(function () {
+			$scope.downloadError = "";
+		}, 6000)
+
+	}
+
+	
+	
+	$scope.editETA={};
+	$scope.searchEditEta = function()
+	{
+		var searchUrl = "/showEditApplicationDetailsDH?applicationId="+$scope.editETA.editEtaApplicationId;
+		
+			$http.get(searchUrl)
+			.then(function (response) {
+				$scope.editETA.editEtaApplicationId = "";
+				
+				if (response.data == null || response.data == "")
+				{
+					$scope.editEtaSearchErrorMessage = "No Records found";
+					$scope.editEtaSearchResult.data = [];
+				} 
+				else
+				{
+					$scope.editEtaSearchErrorMessage = "";
+					$scope.editEtaSearchResult.data = response.data;
+				}
+	
+			});
+	
+			$timeout(function () {
+				$scope.editEtaSearchErrorMessage = "";
+			}, 4000);
+		}
+
+	
+	$scope.editEtaSearchResult = {
+
+			enableGridMenus: false,
+			enableSorting: false,
+			enableFiltering: false,
+			enableCellEdit: false,
+			enableColumnMenus: false,
+			enableHorizontalScrollbar:1,
+			enableVerticalScrollbar: 1,
+			paginationPageSizes: [5, 10, 20, 30],
+			paginationPageSize: 10,
+			useExternalPagination: true,
+
+			columnDefs: [
+				{
+					name: 'applicationId',
+					displayName: 'Post Id',
+					width:'15%'
+				},
+				{
+					name: 'senderName',
+					displayName: "Sender",
+					width: '15%'
+				},
+				{
+					name: 'dateReceived',
+					displayName: 'Date Received',
+					width: '10%'
+				},
+				{
+					name: 'subject',
+					displayName: 'Subject',
+					width: '15%'
+				},
+				{
+					name: 'priority',
+					displayName: 'Priority',
+					width: '10%'
+						
+				},
+				{
+					name: 'ownerName',
+					displayName: 'Owner',
+					width: '10%'
+				},
+				
+				{
+					name: 'dateAssigned',
+					displayName: 'Date Assigned',
+					width: '15%'
+				},
+				{
+					name: 'status',
+					displayName: 'Status',
+					width: '15%'
+				},
+				{
+					name: 'eta',
+					displayName: 'ETA',
+					width: '15%'
+				},
+				{
+					name: 'updatedEta',
+					displayName: 'New ETA',
+					cellTemplate: 'partials/datePickerNewApplication.html',
+					width:'15%'
+				},
+				{
+					name:'update',
+					displayName:'Update ETA',
+					width:'10%',
+					cellTemplate: '<button type="button" class="btn btn-success assignEtaButton" ng-click="grid.appScope.updateEta(row)">Update</button>'
+				},
+				{
+					name: 'documentType',
+					displayName: 'Document Type',
+					width: '15%'
+				},
+				{
+					name: 'documentRemarks',
+					displayName: 'Document Remarks',
+					width: '15%'
+				},
+				{
+					name: 'documentPath',
+					displayName: 'Document',
+					width: '10%',
+					cellTemplate: '<img src="images/pdf.png"  ng-click="grid.appScope.downloadPdf(row.entity.documentPath,row.entity.applicationId)" class="pdfIcon" alt="PDF Icon"  class="pointer">'
+				}
+			]
+
+		};
+
+	
+	$scope.updateEta = function(row)
+	{
+		if(row.entity.updatedEta===undefined||row.entity.updatedEta===null)
+			{
+				$scope.editEtaSuccessMessage="";
+				$scope.editEtaErrorMessage="Please select the new ETA to be updated";
+
+			}
+		
+		else
+			{
+				
+			var fd = new FormData();
+			fd.append("applicationId",row.entity.applicationId);
+			fd.append("senderName",row.entity.senderName);
+			fd.append("subject",row.entity.subject);
+			fd.append("priority",row.entity.priority);
+			fd.append("updatedOwner",row.entity.ownerName);
+			fd.append("eta",row.entity.updatedEta);
+			fd.append("documentRemarks",row.entity.documentRemarks);
+			fd.append("documentPath",row.entity.documentPath);
+			fd.append("updatedStatus",row.entity.status);
+			fd.append("documentType",row.entity.documentType);
+			
+			var url = "/editApplicationEtaDH";
+			
+			$http({
+				method: 'POST',
+				url: url,
+				data: fd,
+				headers: {
+					'Content-Type': undefined
+				},
+				transformRequest: angular.identity,
+				transformResponse: [function (data) {
+					thisIsResponse = data;
+					return data;
+
+				}]
+			}).then(function (response)
+						{
+			
+						if(response.data>0)
+							{
+								$scope.editEtaErrorMessage="";
+								$scope.editEtaSuccessMessage="Application ETA successfully updated";
+								$scope.editEtaSearchResult.data=[];
+							}
+						else
+							{
+								$scope.editEtaSuccessMessage ="";
+								$scope.editEtaErrorMessage = "Application ETA could not be updated";
+
+							}
+				});
+			 
+				
+			
+			}
+		$timeout(function () {
+			$scope.editEtaSuccessMessage = "";
+			$scope.editEtaErrorMessage = "";
+		}, 4000);
+		
+			
+			
+			}
+	
+	
+	
+	
+	
+	
+	
 });
