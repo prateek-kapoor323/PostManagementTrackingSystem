de04774079cfb13ee4f2d3c4395282b3af8f2e1a
prateek-kapoor323 <prateek@smaltandberyl.com>
2019-02-05 12:56:52 +0530

    Author           : Prateek File Modified    : insertUsers.sql Description      : Added user sql for KAMAL SAXENA

--- DBScript/insertUsers.sql
+++ DBScript/insertUsers.sql
@@ -1,2 +1,2 @@
 use scgj_pmts;
-INSERT INTO `users` VALUES (1,'Arpit Sharma','arpit@sscgj.in','arpit123','Assessment and Assurance','DH'),(2,'Tanmay','tanmay@gmail.com','tanmay123','Standard and Research','DH'),(3,'Manohar','manohar@sscgj.in','manohar','Data Entry Operator','DEO');
+INSERT INTO `users` VALUES (1,'Arpit Sharma','arpit@sscgj.in','arpit123','Assessment and Assurance','DH'),(2,'Tanmay','tanmay@gmail.com','tanmay123','Standard and Research','DH'),(3,'Manohar','manohar@sscgj.in','manohar','Data Entry Operator','DEO'),(4,'Kamal Saxena','kamal@sscgj.in','kamal123','Assessment and Assurance','DE');
